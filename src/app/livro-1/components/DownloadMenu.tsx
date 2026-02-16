3270dad34211962c22137b5d467ed0730549d0a5
devtiagoabreu <devtiagoabreu@gmail.com>
2026-02-16 19:53:34 -0300

    feat:Inserindo meditação guiada no livro 1

--- src/app/livro-1/components/DownloadMenu.tsx
+++ src/app/livro-1/components/DownloadMenu.tsx
@@ -7,11 +7,12 @@ export default function DownloadMenu() {
 
   const downloads = [
     { name: 'Ebook (PDF)', icon: 'fa-file-pdf', url: 'https://drive.google.com/file/d/1WqAqdz4hoVTGd1ZPTRzYTw5rSxBjUAAA/view' },
-    { name: 'Audiobook (MP3)', icon: 'fa-headphones', url: 'https://drive.google.com/file/d/1GA4WEwRwR0jVXv6YT2xEPGYrbgOIcTTk/view' },
+    { name: 'Audiobook (MP3)', icon: 'fa-headphones', url: 'https://drive.google.com/file/d/1l-dq4SPb25A2eqyRqEncaFUHZyHNLMJm/view?usp=drive_link' },
     { name: 'Podcast', icon: 'fa-podcast', url: 'https://drive.google.com/file/d/1TvsqSzbafuAzsDQSJPLbke1krHRmlvpl/view' },
     { name: 'Videocast', icon: 'fa-video', url: 'https://drive.google.com/file/d/15kphZuS991O-bJrxS07jaP37tjldr0kQ/view' },
     { name: 'Resumo (PDF)', icon: 'fa-file-alt', url: 'https://drive.google.com/file/d/1-gGQO5QQDQsuB5_CVPzDuuQ2YA809dod/view' },
-    { name: 'Infográfico', icon: 'fa-chart-pie', url: 'https://drive.google.com/file/d/1fq31WVqTLskDU50Bk9qYmkeIXQu8OIro/view' }
+    { name: 'Infográfico', icon: 'fa-chart-pie', url: 'https://drive.google.com/file/d/1fq31WVqTLskDU50Bk9qYmkeIXQu8OIro/view' },
+    { name: 'Meditação Guiada', icon: 'fa-person-meditating', url: 'https://drive.google.com/file/d/13cJLxFRdooENpcYdmr7Hwuv0H-22xK2y/view?usp=drive_link' }
   ]
 
   return (
